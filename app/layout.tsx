675036e34745bfaf3953b1b9f958fb343f6142e9
Jakob <jakob.frenzel@hotmail.com>
2025-12-01 19:49:37 +0100

    actually added vercel Analytics and SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           >
             {children}
           </ThemeProvider>
+          <Analytics />
+          <SpeedInsights/>
         </body>
       </html>
     </>
