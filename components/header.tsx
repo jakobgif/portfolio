6e8d902a19d7a334395c30efe32bcfc0c1fa0f6d
Jakob <jakob.frenzel@hotmail.com>
2025-12-10 14:36:59 +0100

    added theme toggle to header, hidden on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "./ui/breadcrumb";
 import { Button } from "./ui/button";
 import { H2 } from "./ui/typography";
 import React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export function Header({ children, path = "/" }: { children?: React.ReactNode; path?: string; }){
+  const { theme, setTheme } = useTheme()
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   const segments = path.split("/").filter(Boolean);
 
   const defaultNavigation = (
@@ -42,8 +49,14 @@ export function Header({ children, path = "/" }: { children?: React.ReactNode; p
 
   return (
     <header className="w-full flex items-center justify-between px-[10vw] py-4 top-0">
-      <H2>Jakob Frenzel</H2>
-      {children || defaultNavigation}
+      <Link href="/"><H2>Jakob Frenzel</H2></Link>
+      <div className="flex flex-row items-center">
+        {children || defaultNavigation}
+        <Button variant="outline" size="icon" onClick={() => toggleTheme()} className="ml-10 hidden md:inline-flex">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        </Button>
+      </div>
     </header>
   )
 }
