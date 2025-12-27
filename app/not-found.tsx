61e8c44f9c02cf8331376d8b7b4e43b2f4b88833
Jakob <jakob.frenzel@hotmail.com>
2025-12-27 15:38:27 +0100

    reduced stroe width for missing file icon

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ import { TbArrowBackUp } from "react-icons/tb";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <TbFileSad size={100} className="mb-10"/>
+      <TbFileSad size={100} strokeWidth={1.2} className="mb-3"/>
       <H2>Page not found</H2>
       <Badge variant="outline" className="mt-5" asChild>
         <Link href="/" prefetch={true}><TbArrowBackUp />bring me back to safety</Link>
